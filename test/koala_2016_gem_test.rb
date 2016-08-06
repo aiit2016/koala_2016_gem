18566d9c9efdc1bc9312b2355789b5619c81bffd
Koala Cheung <a1624jz@aiit.ac.jp>
2016-08-06 07:31:42 +0000

    add test files

---
+++ test/koala_2016_gem_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class Koala2016GemTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Koala2016Gem::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
