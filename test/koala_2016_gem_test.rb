f7af06aff72eea937d7c4fac30fba3dc4bb3103c
None <a1630ty@aiit.ac.jp>
2016-08-06 07:41:28 +0000

    koala_2016_gem_test

--- test/koala_2016_gem_test.rb
+++ test/koala_2016_gem_test.rb
@@ -8,4 +8,12 @@ class Koala2016GemTest < Minitest::Test
   def test_it_does_something_useful
     assert false
   end
+
+  def test_enough_length     
+    assert_equal(false, @my.enough_length("12"))
+    assert_equal(true,  @my.enough_length("123"))
+    assert_equal(true,  @my.enough_length("12345678"))
+    assert_equal(false, @my.enough_length("123456789"))
+  end
+
 end
